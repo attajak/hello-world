38bce5c629ba4b10eabba9ccabab9588d4907e13
Attajak Janrak <attajak@users.noreply.github.com>
2024-04-05 05:23:49 +0000

    Clean

--- docs/_site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "hello-world",
-        "excerpt":"This is the 1st post.  ","categories": ["uncategoriesed"],
-        "tags": ["Hello","World"],
-        "url": "/uncategoriesed/hello-world/",
-        "teaser": null
-      }]
